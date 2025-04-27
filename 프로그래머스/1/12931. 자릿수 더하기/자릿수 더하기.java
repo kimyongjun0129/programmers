c1154ce800317543e44ae041809344ffe438d221
yongjun <96245652+kimyongjun0129@users.noreply.github.com>
2025-04-27 12:04:52 +0900

    [level 1] Title: 자릿수 더하기, Time: 0.03 ms, Memory: 73.4 MB -BaekjoonHub

--- 프로그래머스/1/12931. 자릿수 더하기/자릿수 더하기.java
+++ 프로그래머스/1/12931. 자릿수 더하기/자릿수 더하기.java
@@ -2,21 +2,14 @@ import java.util.*;
 
 public class Solution {
     public int solution(int n) {
-        int positionNum = Integer.toString(n).length();
-        int multiNum = 1;
         int sum = 0;
         
-          
-        // 자리수를 나누기 위한 수 셋팅
-        for (int i = 1; i<positionNum; i++) {
-            multiNum *= 10;
-        }
-        
-        // 셋팅 값으로 나누기, 나머지 연산을 통해 각 자리의 값 구하기
-        for(int i = 0; i<positionNum; i++) {
-            sum += (n/multiNum);
-            n %= multiNum;
-            multiNum /= 10;
+        while(true) {
+            sum += n%10;
+            
+            if (n<1) break;
+            
+            n/=10;
         }
 
         return sum;
