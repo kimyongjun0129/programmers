cc90e0bf1e0670c432ac5a8f5660bb9309ccfa16
yongjun <96245652+kimyongjun0129@users.noreply.github.com>
2025-04-28 19:03:35 +0900

    [level 1] Title: 자연수 뒤집어 배열로 만들기, Time: 0.06 ms, Memory: 74.3 MB -BaekjoonHub

--- 프로그래머스/1/12932. 자연수 뒤집어 배열로 만들기/자연수 뒤집어 배열로 만들기.java
+++ 프로그래머스/1/12932. 자연수 뒤집어 배열로 만들기/자연수 뒤집어 배열로 만들기.java
@@ -1,9 +1,8 @@
 import java.util.*;
 
 class Solution {
-    public List solution(long n) {
-        // 1. long -> list로 변경 (Long 타입을 list로는 바로 변경하지 못하는거 같음)
-        // 2. 일의자리부터 끝까지 역순으로 계산하여 배열에 저장
+    public List<Long> solution(long n) {
+        // 1. 일의자리부터 끝까지 역순으로 계산하여 배열에 저장
         List<Long> result = new ArrayList<>();
         long temp=0;
         while(true) {
