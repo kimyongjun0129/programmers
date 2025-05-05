c65f317f5b20ac5bf3cc3b9a4f54c7c93ff753a3
yongjun <96245652+kimyongjun0129@users.noreply.github.com>
2025-05-06 00:46:10 +0900

    [level 0] Title: rny_string, Time: 0.06 ms, Memory: 81.6 MB -BaekjoonHub

--- 프로그래머스/0/181863. rny＿string/rny＿string.java
+++ 프로그래머스/0/181863. rny＿string/rny＿string.java
@@ -1,23 +1,20 @@
 class Solution {
     public String solution(String rny_string) {
-        int n = rny_string.length();
-        char[] str = rny_string.toCharArray();
-        
-        for(char word : str) {
-            if(word=='m') n++;
-        }
-        
-        char[] new_str = new char[n];
-        int index = 0;
-        for(int i = 0; i<str.length; i++) {
-            if(str[i] == 'm') {
-                new_str[i+index] = 'r';
-                new_str[i+index+1] = 'n';
-                index++;
-            } else {
-                new_str[i+index] = str[i];
+        // m -> rn
+        StringBuilder builder = new StringBuilder();
+
+        for(int i = 0; i < rny_string.length(); i ++){
+            char word = rny_string.charAt(i);
+
+            if(word == 'm'){
+                builder.append("rn");
             }
+            else{
+                builder.append(word);
+            }
+
         }
-        return String.valueOf(new_str);
+
+        return builder.toString();
     }
 }
