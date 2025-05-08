52d5ff70fb159d8d3275e92711ed3d91a4b3f0ef
yongjun <96245652+kimyongjun0129@users.noreply.github.com>
2025-05-08 09:58:10 +0900

    [level 1] Title: 최소직사각형, Time: 1.83 ms, Memory: 98.7 MB -BaekjoonHub

--- 프로그래머스/1/86491. 최소직사각형/최소직사각형.java
+++ 프로그래머스/1/86491. 최소직사각형/최소직사각형.java
@@ -1,26 +1,11 @@
 class Solution {
     public int solution(int[][] sizes) {
-        int num1 = 0;
-        int num2 = 0;
-        
-        // 최대로 포함해야 되는 수
-        for (int i = 0; i<sizes.length; i++) {
-            int big = 0;
-            int small = 0;
-            
-            // 두 수 대소 비교
-            if (sizes[i][0] > sizes[i][1]) {
-                big = sizes[i][0];
-                small = sizes[i][1];
-            } else {
-                big = sizes[i][1];
-                small = sizes[i][0];
-            }
-            
-            // 값 넣기
-            if (big > num1) num1 = big;
-            if (small > num2) num2 = small;
+        int length = 0, height = 0;
+        for (int[] card : sizes) {
+            length = Math.max(length, Math.max(card[0], card[1]));
+            height = Math.max(height, Math.min(card[0], card[1]));
         }
-        return num1*num2;
+        int answer = length * height;
+        return answer;
     }
 }
