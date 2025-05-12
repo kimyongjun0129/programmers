01a93a01b72071710d9d7748fbace7d275a72967
yongjun <96245652+kimyongjun0129@users.noreply.github.com>
2025-05-12 10:01:00 +0900

    [level 1] Title: 문자열 내 마음대로 정렬하기, Time: 0.40 ms, Memory: 73.5 MB -BaekjoonHub

--- 프로그래머스/1/12915. 문자열 내 마음대로 정렬하기/문자열 내 마음대로 정렬하기.java
+++ 프로그래머스/1/12915. 문자열 내 마음대로 정렬하기/문자열 내 마음대로 정렬하기.java
@@ -1,13 +1,31 @@
-import java.util.Arrays;
-
 class Solution {
     public String[] solution(String[] strings, int n) {
-        Arrays.sort(strings, (s1, s2) -> {
-            if (s1.charAt(n) == s2.charAt(n)) {
-                return s1.compareTo(s2);  // 사전순 정렬
+        int len = strings.length;
+
+        for (int i = 0; i < len - 1; i++) {
+            for (int j = 0; j < len - 1 - i; j++) {
+                char c1 = strings[j].charAt(n);
+                char c2 = strings[j + 1].charAt(n);
+
+                // n번째 문자가 더 크면 자리 바꿈
+                if (c1 > c2) {
+                    swap(strings, j, j + 1);
+                }
+                // n번째 문자가 같으면 사전순 정렬
+                else if (c1 == c2) {
+                    if (strings[j].compareTo(strings[j + 1]) > 0) {
+                        swap(strings, j, j + 1);
+                    }
+                }
             }
-            return Character.compare(s1.charAt(n), s2.charAt(n));  // n번째 문자 기준 정렬
-        });
+        }
+
         return strings;
+    }
+
+    private void swap(String[] arr, int i, int j) {
+        String temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
     }
 }
