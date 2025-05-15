7ee8aa9c3ac49fb98f1d79b3772f7634f9a140e8
yongjun <96245652+kimyongjun0129@users.noreply.github.com>
2025-05-15 19:04:29 +0900

    [level 1] Title: 제일 작은 수 제거하기, Time: 2.41 ms, Memory: 91.9 MB -BaekjoonHub

--- 프로그래머스/1/12935. 제일 작은 수 제거하기/제일 작은 수 제거하기.java
+++ 프로그래머스/1/12935. 제일 작은 수 제거하기/제일 작은 수 제거하기.java
@@ -2,12 +2,10 @@ import java.util.*;
 
 public class Solution {
     public int[] solution(int[] arr) {
-        // 배열의 길이가 1인 경우 [-1] 반환
         if (arr.length == 1) {
             return new int[]{-1};
         }
 
-        // 가장 작은 값 찾기
         int min = arr[0];
         for (int num : arr) {
             if (num < min) {
@@ -15,7 +13,6 @@ public class Solution {
             }
         }
 
-        // 가장 작은 값을 제외하고 새 배열 만들기
         int[] answer = new int[arr.length - 1];
         int index = 0;
         for (int num : arr) {
