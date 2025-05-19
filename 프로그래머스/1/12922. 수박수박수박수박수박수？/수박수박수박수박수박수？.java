153a3d300b6ec96d323b120f2c1591d9915433c6
yongjun <96245652+kimyongjun0129@users.noreply.github.com>
2025-05-19 18:50:21 +0900

    [level 1] Title: 수박수박수박수박수박수?, Time: 0.59 ms, Memory: 71 MB -BaekjoonHub

--- 프로그래머스/1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.java
+++ 프로그래머스/1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.java
@@ -1,16 +1,10 @@
 class Solution {
     public String solution(int n) {
         StringBuilder builder = new StringBuilder();
-        if(n%2==0) {
-            for (int i = 0; i<n/2; i++) {
-                builder.append("수박");
-            }
-        } else {
-            for (int i = 0; i<n/2; i++) {
-                builder.append("수박");
-            }
-            builder.append("수");
+        for (int i = 0; i<n/2; i++) {
+            builder.append("수박");
         }
+        if(n%2!=0) builder.append("수");
         return builder.toString();
     }
 }
