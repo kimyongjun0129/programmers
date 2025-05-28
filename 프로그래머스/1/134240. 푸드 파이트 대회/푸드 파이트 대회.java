5123d7eb65f5cfed6352c93ca090fadc8085c229
yongjun <96245652+kimyongjun0129@users.noreply.github.com>
2025-05-28 09:34:59 +0900

    [level 1] Title: 푸드 파이트 대회, Time: 1.18 ms, Memory: 70.7 MB -BaekjoonHub

---
+++ 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.java
@@ -0,0 +1,19 @@
+class Solution {
+    public String solution(int[] food) {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 1; i<food.length; i++) {
+            int foodPerPerson = food[i]/2;
+            for (int j = 0; j<foodPerPerson; j++) {
+                sb.append(i);
+            }
+        }
+        sb.append(0);
+        for (int i = food.length-1; i>0; i--) {
+            int foodPerPerson = food[i]/2;
+            for (int j = 0; j<foodPerPerson; j++) {
+                sb.append(i);
+            }
+        }
+        return sb.toString();
+    }
+}
