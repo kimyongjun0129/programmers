4f8c8149230012cf3ee4cc293070c174e2b7ef4e
yongjun <96245652+kimyongjun0129@users.noreply.github.com>
2025-09-04 12:37:43 +0900

    [level 0] Title: 수 조작하기 2, Time: 6.49 ms, Memory: 88.4 MB -BaekjoonHub

--- 프로그래머스/0/181925. 수 조작하기 2/수 조작하기 2.java
+++ 프로그래머스/0/181925. 수 조작하기 2/수 조작하기 2.java
@@ -2,8 +2,8 @@ class Solution {
     public String solution(int[] numLog) {
         StringBuilder sb = new StringBuilder();
         
-        for (int i=0; i<numLog.length-1; i++) {
-            int value = numLog[i+1] - numLog[i];
+        for (int i=1; i<numLog.length; i++) {
+            int value = numLog[i] - numLog[i-1];
 
             switch(value) {
                 case 1:
