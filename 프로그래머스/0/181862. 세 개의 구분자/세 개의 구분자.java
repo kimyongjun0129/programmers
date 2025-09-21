eb04850f4d55b9767bfdd997d28b6493161cbfdb
yongjun <96245652+kimyongjun0129@users.noreply.github.com>
2025-09-21 20:02:16 +0900

    [level 0] Title: 세 개의 구분자, Time: 59.35 ms, Memory: 110 MB -BaekjoonHub

--- 프로그래머스/0/181862. 세 개의 구분자/세 개의 구분자.java
+++ 프로그래머스/0/181862. 세 개의 구분자/세 개의 구분자.java
@@ -3,31 +3,18 @@ import java.util.*;
 class Solution {
     public String[] solution(String myStr) {
         List<String> list = new ArrayList<>();
-        StringBuilder sb = new StringBuilder();
         
-        for (char myChar : myStr.toCharArray()) {
-            if(myChar == 'a'|| myChar == 'b' || myChar == 'c') {
-                if(sb.length() != 0) {
-                    list.add(sb.toString());
-                    sb.setLength(0);
-                }
-                continue;
-            }
-            else {
-                sb.append(myChar);
-            }
-        }
-        if (sb.length() != 0) {
-            list.add(sb.toString());
+        String[] answers = myStr.split("[abc]+");
+        
+        for(String answer : answers) {
+            if (!answer.equals("")) list.add(answer);
         }
         
-        if (list.isEmpty()) return new String[] {"EMPTY"};
+        if (list.isEmpty()) return new String[]{"EMPTY"};
         
         String[] answer = new String[list.size()];
         for(int i=0; i<list.size(); i++) {
-            if(!list.get(i).equals("")) {
-                answer[i] = list.get(i);
-            }
+            answer[i] = list.get(i);
         }
         
         return answer;
