516c9233e73241e2ef0bdf0d930c206d01c4d07b
yongjun <96245652+kimyongjun0129@users.noreply.github.com>
2025-10-03 22:43:01 +0900

    [level 0] Title: 캐릭터의 좌표, Time: 0.03 ms, Memory: 92.9 MB -BaekjoonHub

---
+++ 프로그래머스/0/120861. 캐릭터의 좌표/캐릭터의 좌표.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int[] solution(String[] keyinput, int[] board) {
+        
+        int[] answer = new int[2];
+        
+        for(String key: keyinput) {
+            switch(key) {
+                case "left":
+                    if(answer[0]>-(board[0]/2)) answer[0] -= 1;
+                    break;
+                case "right":
+                    if(answer[0]<(board[0]/2)) answer[0] += 1;
+                    break;
+                case "up":
+                    if(answer[1]<(board[1]/2))answer[1] += 1;
+                    break;
+                case "down":
+                    if(answer[1]>-(board[1]/2)) answer[1] -= 1;
+                    break;
+            }
+        }
+        return answer;
+    }
+}
