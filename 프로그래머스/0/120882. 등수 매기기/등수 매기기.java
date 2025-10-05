be070b98b7d9fe0716f31caf3bb7e70dd859336a
yongjun <96245652+kimyongjun0129@users.noreply.github.com>
2025-10-05 21:02:39 +0900

    [level 0] Title: 등수 매기기, Time: 0.04 ms, Memory: 87.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/120882. 등수 매기기/등수 매기기.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int[][] score) {
+        
+        int[] answer = new int[score.length];
+        Arrays.fill(answer, 1);
+        
+        for(int i=0; i<score.length; i++) {
+            for(int j=0; j<score.length; j++) {
+                if(score[i][0] + score[i][1] < score[j][0] + score[j][1]) {
+                    answer[i]++;
+                }
+            }
+        }
+        
+        return answer;
+    }
+}
